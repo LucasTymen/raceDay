f3e87e9f2a4fd8d3208a534beecf44846edd8cb1
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-20 13:35:45 +0100

    raceDay

--- raceDay.js
+++ raceDay.js
@@ -1,29 +1,32 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 let registeredEarly = true;
-let runnersAge = 34;
-let runningTime = '';
+let runnersAge = 43;
+let time = ["9:30 am ", "11:00 am", "12:30 pm "]
+let runningTime = [""];
 
-switch (runningTime){
-  case (runnersAge <= 18):
-    return 'runningTime[2]';
+for (const element of time) {
+ /* console.log(element); */
+}
+
+switch (time){
+  case 0:
+    registeredEarly === true && runnersAge > 18;
+    return 'time[0]';
+    console.log("racing at 9:30 am ")
     break ;
-  case (registeredEarly === true && runnersAge > 18):
-    return 'runningTime[0]';
+  case 1:
+    registeredEarly === false || runnersAge > 18;
+    return 'time[1]';
+    console.log("racing at 11:00 am")
     break ;
-  case (registeredEarly === false && runnersAge > 18):
-    return 'runningTime[1]';
+  case 2:
+    runnersAge <= 18;
+    return 'time[2]';
     break ;
+    console.log("racing at 12:30 pm")
   default:
     console.log('unregistered')
     break ;
-}
-
-if (runnersAge > 18) {
-  console.log("adult");
-} else if (runnersAge <= 18) {
-  console.log("youngster on the borderline");
-} else {
-  console.log("youngster");
 }
 
 if (registeredEarly === true && runnersAge > 18) {
@@ -38,8 +41,27 @@ if (registeredEarly === true && runnersAge > 18) {
   console.log("not ok")
 }
 
+if (runnersAge > 18) {
+  console.log("adult");
+} else if (runnersAge === 18) {
+  console.log("youngster on the borderline");
+} else {
+  console.log("youngster");
+}
+
+console.log(runnersAge, time['element'], raceNumber)
 /*
 Start time:
+
+const array1 = ['a', 'b', 'c'];
+
+for (const element of array1) {
+  console.log(element);
+}
+
+// Expected output: "a"
+// Expected output: "b"
+// Expected output: "c"
 
     Adult registrants run at 9:30 am or 11:00 am.
         Early adults run at 9:30 am.
