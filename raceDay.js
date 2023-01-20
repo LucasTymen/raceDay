92bd0f18a9bd9484183dc7a90c4f88fb0949ec75
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-20 16:05:08 +0100

    raceDay

--- raceDay.js
+++ raceDay.js
@@ -1,44 +1,50 @@
 let raceNumber = Math.floor(Math.random() * 1000);
-let registeredEarly = true;
-let runnersAge = 43;
-let time = ["9:30 am ", "11:00 am", "12:30 pm "]
-let runningTime = [""];
+let registeredEarly = false;
+let runnersAge = 65;
+let time = ["9:30 am ", "11:00 am", "12:30 pm "];
 
 for (const element of time) {
- /* console.log(element); */
+  /* console.log(element); */
 }
 
-switch (time){
+switch (time) {
   case 0:
     registeredEarly === true && runnersAge > 18;
-    return 'time[0]';
-    console.log("racing at 9:30 am ")
-    break ;
+    console.log(time.length[0]);
+    console.log("racing at 9:30 am ");
+    break;
   case 1:
     registeredEarly === false || runnersAge > 18;
-    return 'time[1]';
-    console.log("racing at 11:00 am")
-    break ;
+    console.log(time.length[1]);
+    console.log("racing at 11:00 am");
+    break;
   case 2:
     runnersAge <= 18;
-    return 'time[2]';
-    break ;
-    console.log("racing at 12:30 pm")
+    console.log(time.length[2]);
+    console.log("12:30 pm");
+    break;
+
   default:
-    console.log('unregistered')
-    break ;
+    console.log("");
+    break;
 }
 
-if (registeredEarly === true && runnersAge > 18) {
+let runningTime = time.lengh;
 
-  console.log("Adult and checked early")
-  console.log("race number "+ raceNumber)
-  console.log("Running time :  "+runningTime)
-} else if (registeredEarly === false && runnersAge > 18){
-  runningTime = "11:00 am"
-  console.log()
+if (registeredEarly === true && runnersAge > 18) {
+  console.log("Adult and checked early ");
+  console.log("race number " + raceNumber);
+  console.log("racing at 9:30 am ");
+  console.log("Running time :  " + runningTime + time.length);
+} else if (registeredEarly === false && runnersAge > 18) {
+  console.log("Adult and checked late ");
+  runningTime = "11:00 am";
+  console.log("racing at 11:00 am ");
+} else if (runnersAge <= 18) {
+  console.log("Kid and checked ");
+  console.log("12:30 pm ");
 } else {
-  console.log("not ok")
+  console.log("not ok ");
 }
 
 if (runnersAge > 18) {
@@ -49,22 +55,9 @@ if (runnersAge > 18) {
   console.log("youngster");
 }
 
-console.log(runnersAge, time['element'], raceNumber)
-/*
-Start time:
-
-const array1 = ['a', 'b', 'c'];
-
-for (const element of array1) {
-  console.log(element);
-}
-
-// Expected output: "a"
-// Expected output: "b"
-// Expected output: "c"
-
-    Adult registrants run at 9:30 am or 11:00 am.
-        Early adults run at 9:30 am.
-        Late adults run at 11:00 am.
-    Youth registrants run at 12:30 pm (regardless of registration).
-*/
+console.log(
+  "age :" + runnersAge,
+  time.length,
+  "race number :" + raceNumber,
+  "Taking course at : " + runningTime
+);
